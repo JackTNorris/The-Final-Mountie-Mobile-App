dd6c0c76da45d9dd997fa0aafda46ac196d9b1c5
Jack Norris <48145689+jtnorris2001@users.noreply.github.com>
2019-11-17 23:37:58 -0600

    Update EventList.js

--- components/EventList.js
+++ components/EventList.js
@@ -75,13 +75,13 @@ class EventList extends React.Component {
     var allStuff = [];
     var firebase = require('firebase');
     var config = {
-      apiKey: 'AIzaSyB_JrcF7ogRqsOo2h9jW0GrA5_vSMCdgLA',
-      authDomain: 'mountie-mobile.firebaseapp.com',
-      databaseURL: 'https://mountie-mobile.firebaseio.com',
-      projectId: 'mountie-mobile',
-      storageBucket: 'mountie-mobile.appspot.com',
-      messagingSenderId: '269099119210',
-      appId: '1:269099119210:web:ccbc6e962f9cdbed',
+      apiKey: '******',
+      authDomain: '******',
+      databaseURL: '******',
+      projectId: '******',
+      storageBucket: '******',
+      messagingSenderId: '******',
+      appId: '******',
     };
     if (!firebase.apps.length) {
       //checks to make sure there's only one instance of app
